427ee5db34185f7fb72ed57dc0eb37b96acbabdb
unknown <g.k.kumar9024@gmail.com>
2024-02-26 13:27:02 +0530

    color code change

--- colorchange.js
+++ colorchange.js
@@ -1,13 +1,15 @@
 let btn = document.querySelector("button")
 let body = document.querySelector("body")
-function girraj() {
-    // let random = Math.floor(Math.random() * 16777251)
-    // let getcode = random.toString(16)
-    btn.addEventListener("click", () => {
-        let random = Math.floor(Math.random() * 16777251)
-        let getcode = "#" + random.toString(16)
-        console.log(getcode)
-        body.style.backgroundColor=getcode;
-    })
-}
-girraj()
+let para = document.querySelector("p")
+// btn.addEventListener("click", () => {
+//     let random = Math.floor(Math.random() * 16777251)
+//     let getcode = "#" + random.toString(16)
+//     para.innerText=getcode
+//     body.style.backgroundColor = getcode;
+// })
+btn.addEventListener("click", () => {
+    let random = Math.floor(Math.random() * 256)
+    let getcode = "#" + random
+    para.innerText=getcode
+    body.style.backgroundColor = getcode;
+})
